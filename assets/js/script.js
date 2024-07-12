fc634eb93bae24373eddb9cd11187a399862968c
jack <jackrozovsky@gmail.com>
2024-07-12 14:58:39 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 const api = `AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;
 const searchinput = document.getElementById(`searchinput`);
 const searchButton = document.getElementById(`searchButton`);
-const apiUrl = `https://places.googleapis.com/v1/places/GyuEmsRBfy61i59si0?fields=addressComponents&key=AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;
+const apiUrl = `https://places.googleapis.com/v1/places/GyuEmsRBfy61i59si0?fields=addressComponents&key=AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;       
+
+
+function getdata(){
+    let city = searchinput.value.trim();
+    const apiUrl = `https://places.googleapis.com/v1/places/GyuEmsRBfy61i59si0?fields=addressComponents&key=AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;       
+    fetch(apiUrl)
+   
+    .then(function (response) {
+         return response.json();
+    })
+    
+    .then(function (data) {
+         console.log(data);
+    })
+    
+    .catch(function (error) {
+         console.error(error);
+})
+}
