937cc24b59fcd96aa59a21e190be91c217183331
jack <jackrozovsky@gmail.com>
2024-07-12 14:55:46 -0400

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
-const API = `AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;
+const api = `AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;
 const searchinput = document.getElementById(`searchinput`);
 const searchButton = document.getElementById(`searchButton`);
+const apiUrl = `https://places.googleapis.com/v1/places/GyuEmsRBfy61i59si0?fields=addressComponents&key=AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;
