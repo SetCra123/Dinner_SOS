5b2c6f59f035b17bc1b803eb7a72805972c4f3d7
jack <jackrozovsky@gmail.com>
2024-07-12 13:43:21 -0400

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
+const API = `AIzaSyB2xJTNdvozzeQXnSXSVt9o5hApjh1Jj-s`;
 const searchinput = document.getElementById(`searchinput`);
 const searchButton = document.getElementById(`searchButton`);
-
